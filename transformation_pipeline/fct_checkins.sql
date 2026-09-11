c1036cddd409f095c8440c531e241e8d315b2819
Daniel Bostrom <danjbostrom@gmail.com>
2026-09-11 16:20:29 -0400

    Created fct_checkins table

---
+++ transformation_pipeline/fct_checkins.sql
@@ -0,0 +1,22 @@
+create or refresh materialized view silver.fct_checkins as (
+
+    with
+
+    exploded as (
+        
+        select
+            business_id,
+            explode(split(date, ',')) as checkin_date
+        from genieology.bronze.checkin
+
+    )
+
+    select
+        business_id,
+        count(*) as checkin_count,
+        min(checkin_date::timestamp)::date as first_checkin,
+        max(checkin_date::timestamp)::date as latest_checkin
+    from exploded
+    group by business_id
+
+);
