0fb994de93a064b0ece3468f412169cb2ae32977
Daniel Bostrom <danjbostrom@gmail.com>
2026-09-14 16:33:53 -0400

    Added text field to reviews_and_tips

--- transformation_pipeline/reviews_and_tips.sql
+++ transformation_pipeline/reviews_and_tips.sql
@@ -3,13 +3,15 @@ create or replace materialized view genieology.gold.reviews_and_tips as (
         review_id as text_id,
         business_id,
         'review' as type,
-        posted_at
+        posted_at,
+        review_text as full_text
     from genieology.silver.fct_reviews
     union
     select
         tip_id::varchar(50) as text_id,
         business_id,
         'tip' as type,
-        posted_at
+        posted_at,
+        tip_text as full_text
     from genieology.silver.fct_tips
 );
