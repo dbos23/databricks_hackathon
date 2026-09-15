0e2e2235bf2c5bee51d20e292be4b73c27a0a5a7
Daniel Bostrom <danjbostrom@gmail.com>
2026-09-15 09:50:32 -0400

    Filtered chunked table to New Orleans only

--- transformation_pipeline/reviews_and_tips_chunked.sql
+++ transformation_pipeline/reviews_and_tips_chunked.sql
@@ -1,6 +1,16 @@
 create or refresh materialized view gold.reviews_and_tips_chunked as (
+-- business is already filtered to New Orleans so an inner join will also filter the reviews and tips
+    with new_orleans_only as (
+        select
+            reviews_and_tips.text_id,
+            reviews_and_tips.full_text
+        from genieology.gold.reviews_and_tips
+            inner join genieology.gold.business
+                on reviews_and_tips.business_id = business.business_id
+    )
+
     select
         text_id,
         posexplode(genieology.dev.chunk_by_size(full_text, 150, 20)) as (chunk_id, text_chunk) 
-    from genieology.gold.reviews_and_tips
+    from new_orleans_only
 );
