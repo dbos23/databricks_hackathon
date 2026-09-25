37e571a0825c69af4fa4f15f645e0a1181308761
Daniel Bostrom <danjbostrom@gmail.com>
2026-09-25 10:18:08 -0400

    Added business_id to chunked table

--- transformation_pipeline/gold/reviews_and_tips_chunked.sql
+++ transformation_pipeline/gold/reviews_and_tips_chunked.sql
@@ -1,9 +1,11 @@
-create or refresh materialized view gold.reviews_and_tips_chunked as (
+create or refresh streaming table gold.reviews_and_tips_chunked as (
     select
         reviews_and_tips.text_id,
+        reviews_and_tips.business_id,
         chunks.pos as chunk_id,
+        md5(reviews_and_tips.text_id || '|' || chunk_id) as pk,
         substring(reviews_and_tips.full_text, 1 + chunks.pos * 130, 150) as text_chunk
-    from genieology.gold.reviews_and_tips
+    from stream(genieology.gold.reviews_and_tips)
         join lateral posexplode(
             sequence(0, greatest(0, cast(ceil((length(reviews_and_tips.full_text) - 150.0) / 130.0) as int)))
             ) as chunks(pos, val)
